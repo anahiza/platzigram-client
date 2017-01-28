04a0e850c910fbbc93dca490e3ee1dd756d2fcae
Anahi <anahi.z.a@gmail.com>
2017-01-28 09:14:30 -0600

    Creación de nuestro cliente

---
+++ index.js
@@ -0,0 +1,6 @@
+'use strict'
+const Client = require('./lib/client')
+
+exports.createClient = function (options) {
+  return new Client(options)
+}
---
+++ test/client-test.js
@@ -0,0 +1,16 @@
+'use strict'
+const test = require('ava')
+const platzigram = require('../index')
+// const fixtures = require('./fixtures')
+
+test('client', t => {
+  const client = platzigram.createClient()
+  t.is(typeof client.getPicture, 'function')
+  t.is(typeof client.savePicture, 'function')
+  t.is(typeof client.likePicture, 'function')
+  t.is(typeof client.listPictures, 'function')
+  t.is(typeof client.listPicturesByTag, 'function')
+  t.is(typeof client.saveUser, 'function')
+  t.is(typeof client.getUser, 'function')
+  t.is(typeof client.auth, 'function')
+})
