071661e7ecc11df2c5e80afadff7df7f1c910b00
Anahi <anahi.z.a@gmail.com>
2017-01-28 12:35:33 -0600

    implementando perfil de usuario, 1

--- test/client-test.js
+++ test/client-test.js
@@ -1,10 +1,22 @@
 'use strict'
 const test = require('ava')
 const platzigram = require('../index')
-// const fixtures = require('./fixtures')
+const fixtures = require('./fixtures')
+const nock = require('nock')
+let options = {
+  endpoints: {
+    pictures: 'http://platzigram.test/picture',
+    users: 'http://platzigram.test/user',
+    auth: 'http://platzigram.test/auth'
+  }
+}
+
+test.beforeEach(t => {
+  t.context.client = platzigram.createClient()
+})
 
 test('client', t => {
-  const client = platzigram.createClient()
+  const client = t.context.client
   t.is(typeof client.getPicture, 'function')
   t.is(typeof client.savePicture, 'function')
   t.is(typeof client.likePicture, 'function')
@@ -13,4 +25,14 @@ test('client', t => {
   t.is(typeof client.saveUser, 'function')
   t.is(typeof client.getUser, 'function')
   t.is(typeof client.auth, 'function')
+})
+
+test('getPicture', async t => {
+  const client = t.context.client
+  let image = fixtures.getImage()
+  nock(options.endpoints.pictures)
+    .get(`/${image.publicId}`)
+    .reply(200, image)
+  let result = await client.getPicture(image.publicId)
+  t.deepEqual(image, result)
 })
